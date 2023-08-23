2e9fe4eee98500b6f18b05f2621b8836d5baf0ce
Ｅthan <et-repositories@proton.me>
2023-08-23 17:59:33 +0800

    fix: readme in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 """!/usr/bin/env python"""
 # coding: utf-8
 
+from pathlib import Path
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="fair_random",
-    version="0.0.4",
+    version="0.0.6",
     author="et-repositories",
     author_email="et-repositories@proton.me",
     url="https://github.com/et-repositories/fair-random",
     description="fair random with hash prove",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["fair_random"],
     install_requires=[],
 )
